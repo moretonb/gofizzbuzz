304d2532ac49589112939d2fed2311ea25c57bf5
Moreton Brockley <moretonb@gmail.com>
2016-08-04 22:22:29 +0100

    leverage go routines and channels for main loop

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
-import "fmt"
-import "gofizzbuzz/game"
+import (
+	"fmt"
+	"gofizzbuzz/game"
+	"runtime"
+)
 
 func main() {
+	runtime.GOMAXPROCS(8)
+
+	gameResults := make(chan string)
+	complete := make(chan bool)
+
+	go play1to100(gameResults)
+	go printer(gameResults, complete)
+
+	<-complete
+}
+
+func play1to100(gameResults chan string) {
 	for index := 1; index <= 100; index++ {
-		fmt.Println(game.Play(index))
+		gameResults <- game.Play(index)
 	}
+
+	close(gameResults)
+}
+
+func printer(gameResults chan string, complete chan bool) {
+	for result := range gameResults {
+		fmt.Println(result)
+	}
+
+	complete <- true
 }

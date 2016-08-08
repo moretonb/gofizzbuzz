920de12f50b43d9db25aac774375014bb7d3f264
Moreton Brockley <moretonb@gmail.com>
2016-08-08 03:41:04 +0100

    rename println wrapping function

--- output/factory.go
+++ output/factory.go
@@ -3,9 +3,9 @@ package output
 import "fmt"
 
 func CreatePrinter() IPrinter {
-	return newPrinter(fmtWrapper)
+	return newPrinter(printLnWrapper)
 }
 
-func fmtWrapper(input string) {
+func printLnWrapper(input string) {
 	fmt.Println(input)
 }
